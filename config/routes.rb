3d31861a724ee3e69e868b1a8ddd28cf2256742f
bjwhip <bjwhip@gmail.com>
2017-09-29 12:59:42 -0600

    Fixed rout and whitelist issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'users/index'
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+
 
   root to: 'trash#home'
 end
