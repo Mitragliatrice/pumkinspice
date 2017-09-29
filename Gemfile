65b9a858f018eb1e74c84c99bc4d813cc6c80351
Mark Albright <kalani729@gmail.com>
2017-09-29 15:22:42 -0600

    Added cocoon gem

--- Gemfile
+++ Gemfile
@@ -54,5 +54,5 @@ gem 'elasticsearch', '~> 5.0', '>= 5.0.4'
 
 gem 'devise', '~> 4.3'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+gem 'cocoon', '~> 1.2', '>= 1.2.10'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170929181702) do
+ActiveRecord::Schema.define(version: 20170929201532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +28,6 @@ ActiveRecord::Schema.define(version: 20170929181702) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_skill_lists_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
