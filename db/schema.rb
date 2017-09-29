7f46079c6a53d20fccb7023d9913e91a7e0209a6
Colton Mathews <mathews.colton@gmail.com>
2017-09-29 15:24:00 -0600

    Added index to skills and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170929181702) do
+ActiveRecord::Schema.define(version: 20170929212329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 20170929181702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.index ["title"], name: "index_skill_lists_on_title"
     t.index ["user_id"], name: "index_skill_lists_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170929181702) do
     t.text "github"
     t.text "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
